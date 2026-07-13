a575b42cdffaca28dcf8323a3bb1e422a9bb1585
Dennis van Buuren <dennis_vanbuuren@hotmail.com>
2026-07-13 13:18:41 +0200

    test: add customer service auth test foundation

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,12 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    use HasUuids;
+    /** @use HasFactory<\Database\Factories\UserFactory> */
+    use HasFactory, HasUuids;
 
     protected $fillable = [
         'naam', 'email', 'wachtwoord_hash', 'rol', 'kleur', 'avatar', 'actief',
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,20 @@
 
 namespace Tests;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected function actingAsUser(array $attributes = []): User
+    {
+        $user = User::factory()->create($attributes);
+
+        $this->withSession([
+            'userId' => $user->id,
+            'rol' => $user->rol,
+        ]);
+
+        return $user;
+    }
 }
